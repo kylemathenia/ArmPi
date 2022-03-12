7d3c3b6a8f6bd882b0491d69d6d9f9f55afedfd2
Kyle Mathenia <66032082+kylemathenia@users.noreply.github.com>
2022-03-11 18:07:48 -0600

    Update ROB599_Flight.py

--- Functions/ROB599_Flight.py
+++ Functions/ROB599_Flight.py
@@ -25,13 +25,10 @@ class Flight():
 
     def fly(self):
         self.movement.go_to_initial_position()
-        self._wait_for_aruco()
+        _ = self._wait_for_aruco()
         # Grab a block. Find aruco location.
-        print('here1')
         self.movement.grasp_obj_at_pose(self.movement.poses['blue'], 'cube', 'ground', lift=True)
-        print('here2')
-        aruco_loc = self.aruco.detectMarkerXYPosition()
-        print('here3')
+        aruco_loc = self._wait_for_aruco()
         # Place block on truck.
         self.movement.drop_obj_in_loc(aruco_loc, 'cube', obj_height='truck')
         self.movement.go_to_initial_position()
@@ -42,6 +39,7 @@ class Flight():
         aruco_loc = None
         while aruco_loc is None:
             aruco_loc = self.aruco.detectMarkerXYPosition()
+        return aruco_loc
 
 if __name__ == "__main__":
     f = Flight()
